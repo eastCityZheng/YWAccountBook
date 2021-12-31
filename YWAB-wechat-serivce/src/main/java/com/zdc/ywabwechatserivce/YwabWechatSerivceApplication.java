55e32a655e463d2644c8558d06fbeec8be79386d
dczheng <dczheng@travelsky.com.cn>
2021-12-31 14:21:43 +0800

    Initial commit

---
+++ YWAB-wechat-serivce/src/main/java/com/zdc/ywabwechatserivce/YwabWechatSerivceApplication.java
@@ -0,0 +1,13 @@
+package com.zdc.ywabwechatserivce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YwabWechatSerivceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YwabWechatSerivceApplication.class, args);
+    }
+
+}
---
+++ YWAB-wechat-serivce/src/test/java/com/zdc/ywabwechatserivce/YwabWechatSerivceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zdc.ywabwechatserivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YwabWechatSerivceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
